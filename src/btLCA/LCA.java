0a27c1ec3cc4a5dc0d3500c602dac2f41bb4fbcf
thompsco <thompsco@tcd.ie>
2019-09-28 15:51:54 +0100

    Initial commit

---
+++ src/btLCA/LCA.java
@@ -0,0 +1,15 @@
+package btLCA;
+
+public class LCA {
+	public class Node {
+		int val;
+		Node left;
+		Node right;
+		
+		Node(int val) {
+			this.val = val;
+			this.left = null;
+			this.right = null;
+		}
+	}
+}
