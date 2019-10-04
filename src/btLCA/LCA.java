6cf60c9c0bb7a810dbd5f28b1a477d6201532e80
thompsco <thompsco@tcd.ie>
2019-10-04 19:05:00 +0100

    Added methods to LCA.java to find LCA of two nodes in a Binary Tree
    findLowestCommonAncestor returns the value of the lowest common ancestor of pointA and pointB. It calls the method lowestCARecursive which performs the search for the lowest common ancestor of the two nodes in question.

--- src/btLCA/LCA.java
+++ src/btLCA/LCA.java
@@ -2,7 +2,13 @@ package btLCA;
 
 public class LCA {
 
-	public class Node {
+	// Variable names for the two nodes whose Lowest Common Ancestor is to be found
+	int pointA;
+	int pointB;
+	int lowestCommonAncestor;
+	BT binaryTree;
+
+  class BT {
 		int val;
 		Node left;
 		Node right;
@@ -57,4 +63,37 @@ public class LCA {
 			inorderRecursive(root);
 		}
 	}
+
+	public LCA(int pointA, int pointB, BT binaryTree) {
+		this.pointA = pointA;
+		this.pointB = pointB;
+		this.binaryTree = binaryTree;
+		this.lowestCommonAncestor = lowestCommonAncestor;
+	}
+
+	public int findLowestCommonAncestor() {
+		// Traverse the tree while both nodes being checked are to the same side of the
+		// current node being searched against, when the two nodes pointA and pointB are
+		// on opposite sides, the LCA has been found
+		int lowestCA = lowestCARecursive(binaryTree.root).val;
+		return lowestCA;
+	}
+
+	public BT.Node lowestCARecursive(BT.Node node) {
+		if(node == null) {
+			return null;
+		}
+		if(pointA > node.val && pointB > node.val) {
+			return lowestCARecursive(node.right);
+		} else if(pointA < node.val && pointB < node.val) {
+			return lowestCARecursive(node.left);
+		} else {
+			return node;
+		}
+	}
+
+	public int returnLCA() {
+		return lowestCommonAncestor;
+	}
+
 }
